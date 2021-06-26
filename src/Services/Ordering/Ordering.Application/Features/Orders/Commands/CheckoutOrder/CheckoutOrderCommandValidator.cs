ad920810c8bdaed530563c05c15035bedf581dcc
Ricardo Cunha <RicardoCunha@criticalmanufacturing.com>
2021-06-26 19:30:07 +0100

    Ordering.Application layer: wip

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty()
+                .WithMessage("UserName is required")
+                .NotNull()
+                .MaximumLength(50)
+                .WithMessage("UserName must not exceed 50 chars");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty()
+                .WithMessage("Email should not be empty");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("Total price is required")
+                .GreaterThan(0).WithMessage("Total price should be greater than zero");
+        }
+    }
+}
