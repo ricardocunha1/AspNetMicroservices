74aa7198d23040dee891cbd1ac65a3191ce5b42a
Ricardo Cunha <RicardoCunha@criticalmanufacturing.com>
2021-06-27 23:31:06 +0100

    Ordering.Infrastructure: wip

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Database seed finished.");
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order()
+                {
+                    UserName = "ricardocunha",
+                    FirstName = "Ricardo",
+                    LastName = "Cunha",
+                    Country = "PT",
+                    EmailAddress = "ricardo.cunha.mieic@gmail.com",
+                    TotalPrice = 100
+                }
+            };
+        }
+    }
+}
