aab5cbf67bae4f250b2930f89568ce9cbde6b275
Ricardo Cunha <RicardoCunha@criticalmanufacturing.com>
2021-07-04 22:33:14 +0100

    Final version: mass transit message handler

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await _mediator.Send(command);
+
+        }
+    }
+}
